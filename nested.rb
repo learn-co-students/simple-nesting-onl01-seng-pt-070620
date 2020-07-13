6efcb44190b0af754aa3c47581289562f4199652
Andy Adams <andrewdcadams@gmail.com>
2020-07-13 20:59:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+    return programmer_hash[:dennis_ritchie][:languages].join
+     
 end
 
 def adding_matz
@@ -79,9 +84,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     
+     programmer_hash[:yukihiro_matsumoto] = {}
+     return programmer_hash
 end
+
+
 
 def changing_alan
 	programmer_hash = 
@@ -101,9 +109,14 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
      
 end
+
+
+
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +134,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    return programmer_hash
 
 end
