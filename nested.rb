4c45fe37ac853db85679f12019a713b2c549fb6d
Knika Dass <knikadass@gmail.com>
2020-07-17 17:29:10 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -38,8 +38,7 @@ def alan_kay_is_known_for
         }
      }
 
-    programmer_hash[:alan_kay][:known_for] = alans_new_info
-    programmer_hash
+    programmer_hash[:alan_kay][:known_for] 
 end
 
 def dennis_ritchies_language
@@ -105,26 +104,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable
+     
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
     
-     programmer_hash[:alan_kay][:known_for][:alans_new_info] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
      programmer_hash
 
