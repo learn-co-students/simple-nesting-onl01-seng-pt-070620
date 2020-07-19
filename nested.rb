8c9511836bfcc0e9e852cd9c55885809854215f8
Eugene White <eugenewhite1994@gmail.com>
2020-07-19 02:01:08 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -16,11 +15,9 @@ def hopper
         }
      }
 
-
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -58,13 +55,12 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-	programmer_hash = 
+programmer_hash = 
+	
+	:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP, C"]
+}
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -99,7 +95,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     
      alans_new_info = "GUI"
      
      
