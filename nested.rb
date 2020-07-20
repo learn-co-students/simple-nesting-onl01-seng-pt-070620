c886ad5dc99544199b67b9a53d099348d08f5db9
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-20 02:47:35 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,7 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay] [:known_for]
 end
+alan_kay_is_known_for
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,7 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+ programmer_hash[:dennis_ritchie] [:languages].first
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -79,9 +83,15 @@ def adding_matz
           :languages => ["C"]
         }
      }
+programmer_hash[:yukihiro_matsumoto] = 
+{
+  known_for: "Ruby",
+  languages: 
+  ["LISP", "C"]
+}
+programmer_hash
+end 
 
-    
-end
 
 def changing_alan
 	programmer_hash = 
@@ -101,9 +111,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
-end
+     programmer_hash[:alan_kay] [:known_for] = "GUI"
+      programmer_hash
+ end
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie] [:languages] << "Assembly"
+programmer_hash
 end
